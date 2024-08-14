0f25e9a59de50de6313ea2b9753777463df93aa8
Maksim Moiseenkov <maksim_moiseenkov@epam.com>
2024-08-14 14:47:25 +0000

    Refactor DataprocCreateBatchOperator and Dataproc system tests

--- tests/system/providers/google/cloud/dataproc/example_dataproc_gke.py
+++ tests/system/providers/google/cloud/dataproc/example_dataproc_gke.py
@@ -31,6 +31,8 @@ from __future__ import annotations
 import os
 from datetime import datetime
 
+from google.api_core.retry import Retry
+
 from airflow.models.dag import DAG
 from airflow.providers.google.cloud.operators.dataproc import (
     DataprocCreateClusterOperator,
@@ -101,6 +103,7 @@ with DAG(
         project_id=PROJECT_ID,
         location=REGION,
         body=GKE_CLUSTER_CONFIG,
+        result_retry=Retry(maximum=100.0, initial=10.0, multiplier=1.0),
     )
 
     # [START how_to_cloud_dataproc_create_cluster_operator_in_gke]
@@ -110,6 +113,7 @@ with DAG(
         region=REGION,
         cluster_name=CLUSTER_NAME,
         virtual_cluster_config=VIRTUAL_CLUSTER_CONFIG,
+        result_retry=Retry(maximum=100.0, initial=10.0, multiplier=1.0),
     )
     # [END how_to_cloud_dataproc_create_cluster_operator_in_gke]
 
