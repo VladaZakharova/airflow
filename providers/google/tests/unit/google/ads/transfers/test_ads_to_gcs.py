e88250293d03ad1e04105baecedb2cee3c199fe4
Anton Nitochkin <nitochkin@google.com>
2025-03-20 13:40:53 +0000

    Remove deprecated for February and March 2025 in providers/google. Deprecation period for LifeScienceHook extended to july 08, 2025.

--- providers/google/tests/unit/google/ads/transfers/test_ads_to_gcs.py
+++ providers/google/tests/unit/google/ads/transfers/test_ads_to_gcs.py
@@ -54,9 +54,7 @@ class TestGoogleAdsToGcsOperator:
             google_ads_conn_id=google_ads_conn_id,
             api_version=api_version,
         )
-        mock_ads_hook.return_value.search.assert_called_once_with(
-            client_ids=CLIENT_IDS, query=QUERY
-        )
+        mock_ads_hook.return_value.search.assert_called_once_with(client_ids=CLIENT_IDS, query=QUERY)
         mock_gcs_hook.assert_called_once_with(
             gcp_conn_id=gcp_conn_id,
             impersonation_chain=IMPERSONATION_CHAIN,
